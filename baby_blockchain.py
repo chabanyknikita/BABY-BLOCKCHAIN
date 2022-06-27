a0e82a7bb501ba65c0332a21825b772379adc039
Nikita Chabaniuk <92546152+chabanyknikita@users.noreply.github.com>
2022-06-28 00:50:15 +0300

    Update baby_blockchain.py

--- baby_blockchain.py
+++ baby_blockchain.py
@@ -58,3 +58,60 @@ class Signature:
         message = 'Signature RSA'
         signature = Signature().signData(message.encode(encoding='utf-8'), private_key.read())
         print(signature)
+        
+class Account:
+    def __init__(self):
+        key = RSA.generate(2048)
+        self._private_key = key.export_key()
+        self._public_key = key.publickey().export_key()
+        self._signer = PKCS1_v1_5.new(self._private_key)
+        self._wallet = list()
+        self._balance = int
+
+    def genAccount(self):
+        return hex(int.from_bytes(self._public_key, "little"))
+
+    def addKeyPairToWallet(self):
+        file_out = open("private_key.pem", "wb")
+        file_out.write(self._private_key)
+        file_out.close()
+
+        file_out = open("public_key.pem", "wb")
+        file_out.write(self._public_key)
+        file_out.close()
+
+        self._wallet = [self._private_key, self._public_key]
+        return self._wallet
+
+    def updateBalance(self, b):
+        self._balance = b
+
+    def getBalance(self):
+        return self._balance
+
+    def printBalance(self):
+        print(self._balance)
+
+    def signature(self, plaintext, key, hash_algorithm=Crypto.Hash.SHA512):
+        signer = PKCS1_v1_5.new(RSA.importKey(key))
+        hash_value = hash_algorithm.new(plaintext)
+        return signer.sign(hash_value)
+
+    def verifySignature(self, sign, plaintext, key, hash_algorithm=Crypto.Hash.SHA512):
+        hash_value = hash_algorithm.new(plaintext)
+        verifier = PKCS1_v1_5.new(RSA.importKey(key))
+        return verifier.verify(hash_value, sign)
+
+    def signData(self):
+        message = input("Write message which you want to signature:")
+        signature = Account().signature(message.encode(encoding='utf-8'), self._private_key)
+        return Account().verifySignature(signature, message.encode('utf-8'), self._public_key)
+
+    def createPaymentOp(self, Account, amount, index):
+        if self._balance > amount:
+            return collections.OrderedDict({
+                'recipient': Account,
+                'value': amount,
+                'index': index})
+        else:
+            return "YOU DONT HAVE ENOUGH MONEY!"
